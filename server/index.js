c69d3384d40304ec200ffdce4e020952649220a9
Weariess <125036889+Weariess@users.noreply.github.com>
2023-09-15 14:57:15 +0200

    yoyoyoyo

--- server/index.js
+++ server/index.js
@@ -1,15 +1,48 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql')
 
 
 const cors = require('cors')
 app.use(cors())
 
-const port = 3002
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "szkola"
+})
+
+const port = 3000
+//http://localhost:3000/
 
 app.get('/', function(req, res){
     res.send("ok")
     console.log('done')
 })
-//http://localhost:3002/
+
+app.get('/imie', function(req,res){
+    res.send({"imie":"Wiktoria","nazwisko":"Duciak"})
+})
+
+app.get('/sum/:l1/:l2', function(req,res){
+    const l1 = parseInt(req.params.l1)
+    const l2 = parseInt(req.params.l2)
+    
+    
+    const cyferki = l1 + l2
+    
+    
+    res.send(cyferki.toString())
+})
+
+app.get('/sql', function(req,res){
+    const sql = "SELECT * FROM klasa"
+    con.query(sql,function(err,result,fields){
+        if(err) console.log(err)
+        console.log(fields)
+        res.send(result)
+    })
+})
+
 app.listen(port)
